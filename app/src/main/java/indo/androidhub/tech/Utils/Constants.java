ba5f099794b78ec3a8538d9f5afe2f14b40b2e03
hlsunil <sunil.h.l@hubzu.com>
2017-10-14 21:16:31 +0530

    10

---
+++ app/src/main/java/indo/androidhub/tech/Utils/Constants.java
@@ -0,0 +1,10 @@
+package indo.androidhub.tech.Utils;
+
+/**
+ * Created by sunilhl on 30/08/17.
+ */
+
+public class Constants {
+
+    public static final String SELECTED_KEY = "selected_key";
+}
---
+++ app/src/main/java/indo/androidhub/tech/model/Status.java
@@ -0,0 +1,24 @@
+package indo.androidhub.tech.model;
+
+/**
+ * Created by sunilhl on 29/08/17.
+ */
+
+public class Status {
+    private String title;
+
+    public Status() {
+    }
+
+    public Status(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+}
